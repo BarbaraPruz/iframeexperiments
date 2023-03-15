d9eeb7466950556707ce6e14b54da37ea9d9d6dc
Barbara <barbara.prusiewicz@gmail.com>
2023-03-15 10:02:30 -1000

    Use 4/3 aspect ratio

--- scripts/loadiframe.js
+++ scripts/loadiframe.js
@@ -1,14 +1,23 @@
 window.onload = () => {
   const iframeContainerElement = document.getElementById("iframe-container");
-  const parent = iframeContainerElement.parentNode.parentElement;
-  console.log("Parent", parent);
-  console.log("Parent w,h", parent.clientWidth, parent.clientHeight);
+  //  const parent = iframeContainerElement.parentNode.parentElement;
+  const windowHeight = window.innerHeight;
+  const windowWidth = window.innerWidth;
+  const minWidth = 500; //windowWidth < 500 ? 500 : windowWidth;
+  const minHeight = 500; //windowHeight < 500 ? 500 : windowHeight;
+  const maxHeight = windowHeight;
+  const maxWidth = windowWidth;
+  // console.log("Parent", parent);
+  //  console.log("Parent w,h", parent.clientWidth, parent.clientHeight);
+  //console.log("window w,h", windowWidth, windowHeight);
+  // console.log("min w,h values", minWidth, minHeight);
   //   const width = parent.clientWidth;
   //   const height = parent.clientHeight;
   const width = iframeContainerElement.clientWidth;
   const height = iframeContainerElement.clientHeight;
-  const adjustedWidth = width >= 500 ? `${width}px` : "500px";
-  const adjustedHeight = height >= 500 ? `${height}px` : "500px";
+  const adjustedWidth = width >= minWidth ? width : minWidth;
+  //  const adjustedHeight = height >= minHeight ? `${height}px` : `${minHeight}px`;
+  // const adjustedHeight = adjustedWidth * (16 / 9);
   console.log(
     "onload width, height and adjustments",
     width,
@@ -16,8 +25,8 @@ window.onload = () => {
     adjustedWidth,
     adjustedHeight
   );
-  iframeContainerElement.style.width = adjustedWidth;
-  iframeContainerElement.style.height = adjustedHeight;
+  iframeContainerElement.style.width = `${adjustedWidth}px`;
+  // iframeContainerElement.style.height = `${adjustedHeight}px`;
 
   /*     window.addEventListener("resize", (event) => {
       console.log("resize", window.innerHeight, window.innerWidth);
