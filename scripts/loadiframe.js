cab03eeecdbdf45e697421cce06f93bfd1e93978
Barbara <barbara.prusiewicz@gmail.com>
2023-03-17 15:42:03 -1000

    more script iframe size calculation refinements

--- scripts/loadiframe.js
+++ scripts/loadiframe.js
@@ -1,37 +1,38 @@
 window.onload = () => {
   const iframeContainerElement = document.getElementById("iframe-container");
-  //  const parent = iframeContainerElement.parentNode.parentElement;
-  const windowHeight = window.innerHeight;
-  const windowWidth = window.innerWidth;
-  const minWidth = 500; //windowWidth < 500 ? 500 : windowWidth;
-  const minHeight = 500; //windowHeight < 500 ? 500 : windowHeight;
-  const maxHeight = windowHeight;
-  const maxWidth = windowWidth;
-  // console.log("Parent", parent);
-  //  console.log("Parent w,h", parent.clientWidth, parent.clientHeight);
-  //console.log("window w,h", windowWidth, windowHeight);
-  // console.log("min w,h values", minWidth, minHeight);
-  //   const width = parent.clientWidth;
-  //   const height = parent.clientHeight;
+  const iframeElement = document.getElementById("responsive-iframe");
+
+  const minWidth = 800;
+  const minHeight = 750;
   const width = iframeContainerElement.clientWidth;
   const height = iframeContainerElement.clientHeight;
-  const adjustedWidth = width >= minWidth ? width : minWidth;
-  //  const adjustedHeight = height >= minHeight ? `${height}px` : `${minHeight}px`;
-  // const adjustedHeight = adjustedWidth * (16 / 9);
+  let adjustedWidth;
+  let adjustedHeight;
+  let aspectRatio;
+  if (width < minWidth) {
+    adjustedWidth = `100vw`;
+    adjustedHeight = `100vh`;
+  } else {
+    adjustedWidth = `${width}px`;
+    if (width * (4 / 3) > minHeight) {
+      aspectRatio = "4/3";
+    } else {
+      adjustedHeight = minHeight;
+    }
+  }
+
   console.log(
-    "onload width, height and adjustments",
+    "onload width, height, and settings",
     width,
     height,
     adjustedWidth,
-    adjustedHeight
+    adjustedHeight,
+    aspectRatio
   );
-  // To Do: on mobile, maybe full height and so different aspect ratio?
-  iframeContainerElement.style.width = `${adjustedWidth}px`;
-  // iframeContainerElement.style.height = `${adjustedHeight}px`;
 
-  /*     window.addEventListener("resize", (event) => {
-      console.log("resize", window.innerHeight, window.innerWidth);
-      iframeContainerElement.style.width = window.innerWidth;
-      iframeContainerElement.style.height = window.innerHeight;
-    }); */
+  iframeContainerElement.style.width = adjustedWidth;
+  if (adjustedHeight) iframeContainerElement.style.height = adjustedHeight;
+  if (aspectRatio) {
+    iframeElement.style.aspectRatio = aspectRatio;
+  }
 };
